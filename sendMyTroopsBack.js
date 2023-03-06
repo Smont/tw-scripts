48dd320f4c53876ed6d9bcaebebbe41af34dfb55
Simone Montagnani <montagnani.si@gmail.com>
2023-03-06 00:56:29 -0300

    fix compatibilit
    Fix match url 
    Feat reset btn
    Feat disable btn

--- sendMyTroopsBack.js
+++ sendMyTroopsBack.js
@@ -1,40 +1,34 @@
 // ==UserScript==
 // @name         Retirada de tropas
-// @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
+// @description  https://github.com/Smont/tw-scripts
 // @author       @montagnanii
-// @match        https://br*.tribalwars.com.br/game.php?*&screen=info_village*
+// @match        *screen=info_village*
+// @include        https://br*.tribalwars.com.br/game.php?*screen=info_village*
 // @icon         https://dsbr.innogamescdn.com/asset/59fb2ca0/graphic/icons/header.png
 // @grant        none
 
-// @run-at       document-start
+// @downloadURL none
+// @updateURL   none
+// @run-at document-end
+
 // ==/UserScript==
 
-(function() {
-    'use strict';
 
- //function sendMyTroopsBack() {
+'use strict';
+console.log('inicio')
+
+//function sendMyTroopsBack() {
 var bbody = document.querySelector('form#report_table');
 var checkbox = document.querySelectorAll("input[type=checkbox]");
 
 
-bbody.insertAdjacentHTML("beforeend", "<hr/>\n\
-                                        <table id='bodTable'><tr><td valign='middle' colspan='3'>\n\
-                                                </td><h3>Retirada de tropas - Instrução</h3>\n\
-                                                <p>O script funcionará <b>apenas</b> para as suas próprias aldeias</p>\n\
-                                                <p>Coloque o número de tropas que você quer que fique na aldeia</p>\n\
-                                                <p><b>Ex:</b><br>Tenho x aldeias apoiando com y numeros de lanças, quero que o \n\
-                                                máximo de tropa estacionada seja 250, então colocarei 250 no campo 'lanceiro'. <br>\n\
-                                            </p><p>Se você deixar o campo vazio, ele entenderá que não tirará nenhuma tropa do tipo, caso tenha \n\
-                                                MENOS tropas estacionadas do que 250, ele deixará o total (se tiver 100, ele deixará 100)</p>\n\
-                                                <hr/>\n\
-                                                </tr></table><hr/>\n\
-                                        <div style='margin-top:14px'>\n\
-                                                    <button class='btn' type ='submit' onclick='inputValuesSub()' > Tirar quantidade'</button>\n\
-                                        </div>");
+bbody.insertAdjacentHTML("beforeend", "<hr/><table id='bodTable'><tr><td valign='middle' colspan='3'></td><h3 id='locasw'>Retirada de tropas - Instrução</h3><p>O script funcionará <b>apenas</b> para as suas próprias aldeias</p><p>Coloque o número de tropas que você quer que fique na aldeia</p><p><b>Ex:</b><br>Tenho x aldeias apoiando com y numeros de lanças, quero que o máximo de tropa estacionada seja 250, então colocarei 250 no campo 'lanceiro'. <br>                                            </p><p>Se você deixar o campo vazio, ele entenderá que não tirará nenhuma tropa do tipo, caso tenha MENOS tropas estacionadas do que 250, ele deixará o total (se tiver 100, ele deixará 100)</p><hr/></tr></table><hr/><div style='margin-top:14px'><button class='btn'id='submitItens' > Tirar quantidade'</button><button class='btn'id='resetItens' disabled> Resetar '</button></div>");
 
-var _table = document.querySelector('#bodTable')
+var _table = document.querySelector('#bodTable');
+// var get = location.href;
+// location.href = "#locasw";
+
 var units = [
     {
         twName: "spear",
@@ -86,13 +80,12 @@ units.forEach((u, i) => {
     _table.insertAdjacentHTML("beforeend", "<tr><td valign='middle'  width='20px'><img src='https://dsbr.innogamescdn.com/asset/59fb2ca0/graphic/unit/unit_" + u.twName + ".png'</td><td valign='middle'><span id='unit_" + i + "'>" + u.alias + ":<td valign='middle'><input id='id_" + u.twName + "' type='number' value='' name='[units][" + u.twName + "]' /></span><td valign='middle'> </td></tr>");
 });
 
-
-
-function inputValuesSub() {
+document.getElementById("submitItens").addEventListener("click", function () {
 
     event.preventDefault();
 
-    // document.getElementById("units_select_all").click();
+
+    document.getElementById("units_select_all").click();
 
     for (let check of checkbox) {
         if (check.name.includes("send_back")) {
@@ -113,9 +106,10 @@ function inputValuesSub() {
     let valSpear = document.querySelector("input[id='id_spear']").value;
 
     let archer = "[units][archer]";
-let valArcher = '';
+    let valArcher = ''
+
     if (document.querySelector("input[id='id_archer']")) {
-         valArcher = document.querySelector("input[id='id_archer']").value;
+        valArcher = document.querySelector("input[id='id_archer']").value;
 
     }
 
@@ -124,52 +118,50 @@ let valArcher = '';
 
         if (item.name.includes(sword)) {
 
-            if (valSword !== '' && valSword !== '' && item.value > valSword) {
+            if (valSword !== '' && item.value > valSword) {
                 item.value = item.value - valSword;
                 item.style.backgroundColor = "lightgreen";
+                item.style.border = "1px solid darkgreen";
             } else {
                 item.value = 0;
-                item.style.backgroundColor = "red";
+                item.style.backgroundColor = "lightred";
             }
         } else if (item.name.includes(heavy)) {
-            if (valSword !== '' && item.value > valHeavy) {
+            if (heavy !== '' && item.value > valHeavy) {
                 item.value = item.value - valHeavy;
                 item.style.backgroundColor = "lightgreen";
+                item.style.border = "1px solid darkgreen";
             } else {
                 item.value = 0;
-                item.style.backgroundColor = "red";
+                item.style.backgroundColor = "lightred";
             }
         } else if (item.name.includes(spear)) {
 
-            if (valSpear !== '' && valSword !== '' && item.value > valSpear) {
+            if (valSpear !== '' && item.value > valSpear) {
                 item.value = item.value - valSpear;
                 console.log('valor', item.value)
                 item.style.backgroundColor = "lightgreen";
+                item.style.border = "1px solid darkgreen";
             } else {
                 item.value = 0;
-                item.style.backgroundColor = "red";
+                item.style.backgroundColor = "lightred";
             }
         } else if (item.name.includes(archer)) {
-            if (valSword !== '' && item.value > valArcher) {
+            if (valArcher !== '' && item.value > valArcher) {
                 item.value = item.value - valArcher;
                 item.style.backgroundColor = "lightgreen";
+                item.style.border = "1px solid darkgreen";
             } else {
                 item.value = 0;
-                item.style.backgroundColor = "red";
+                item.style.backgroundColor = "lightred";
             }
         } else {
             item.value = 0;
         }
-
-
-
-
     }
-}
 
 
+    document.getElementById('submitItens').setAttribute('disabled', true);
+    document.getElementById('resetItens').removeAttribute('disabled');
 
-
-
-    //}
-})();
+}); 
